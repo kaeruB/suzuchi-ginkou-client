66c74579713fde659da50e74c4f229c326f55e72
kazoogon <kakazoo209@gmail.com>
2021-09-12 19:47:52 +0200

    add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import Dashboard from "./Dashboard";
+import Dashboard from './Dashboard'
 
 const Home: NextPage = () => {
   return (
@@ -13,10 +13,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          鈴地銀行
-        </h1>
-          <Dashboard/>
+        <h1 className={styles.title}>鈴地銀行</h1>
+        <Dashboard />
       </main>
     </div>
   )
