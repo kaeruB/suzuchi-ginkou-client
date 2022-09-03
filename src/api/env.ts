354ec5f37629471f7670bf1c26b6a25cc81f8ef3
Agata Bogacz <agatabogacz@outlook.com>
2022-09-03 16:51:29 +0200

    Set default server name to german server.

--- src/api/env.ts
+++ src/api/env.ts
@@ -1,5 +1,6 @@
-export const LOCALHOST = 'http://localhost:3005'
+export const SUZUCHI_GINKOU_GERMAN_SERVER = 'http://139.177.178.87:3005'
+// export const LOCALHOST = 'http://localhost:3005'
 
-export const DEFAULT_HOSTNAME = LOCALHOST
+export const DEFAULT_HOSTNAME = SUZUCHI_GINKOU_GERMAN_SERVER
 
 export const DEFAULT_HISTORY_ITEMS = 5
